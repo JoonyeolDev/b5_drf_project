9de80f7adcc8b437c9966b1c14e66deead469310
Kim Gwang woon <misili304@naver.com>
2023-05-08 15:44:37 +0900

    Add/ Posting URL patterns

---
+++ posts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.PostingView.as_view, name="posting-list"),
+    path("create/", views.PostingCreateView.as_view, name="posting-create"),
+    path("<int:posting_id>/", views.PostingDetailView.as_view, name="posting-detail"),
+]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from posts.models import Posting
 
 # Create your views here.
+
+
+class PostingView(APIView):
+    """
+    게시글 리스트 모두 보여주기(작성 시간순으로 정렬)
+    추후 페이지네이션 추가
+    """
+    def get(self, request):
+        pass
+    """
+    게시글 작성기능(모달창 사용시)
+    """
+    def post(self, request):
+        pass
+
+
+class PostingCreateView(APIView):
+    """
+    게시글 작성 페이지 띄워주기
+    """
+    def get(self, request):
+        pass
+    """
+    게시글 작성 요청하기
+    """
+    def post(self, request):
+        pass
+
+
+class PostingDetailView(APIView):
+    def get(self, request):
+        pass
+    def post(self, request):
+        pass
+    def put(self, request):
+        pass
+    def delete(self, request):
+        pass
