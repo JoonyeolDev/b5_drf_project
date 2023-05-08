23f4bbf6825bdc9296624c75456da14d08872def
jeanallen928 <123160000+jeanallen928@users.noreply.github.com>
2023-05-08 22:51:53 +0900

    add token login

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from users import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 
 urlpatterns = [
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('signup/', views.UserView.as_view(), name="user_view"),
 ]
