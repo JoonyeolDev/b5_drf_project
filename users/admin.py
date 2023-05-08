ff081c6c3a4c57bd0130f73d8d9e6baf3ec74708
jeanallen928 <123160000+jeanallen928@users.noreply.github.com>
2023-05-08 20:20:11 +0900

    create admin page

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,81 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.core.exceptions import ValidationError
 
-# Register your models here.
+from users.models import User
+
+
+class UserCreationForm(forms.ModelForm):
+
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label="Password confirmation", widget=forms.PasswordInput
+    )
+
+    class Meta:
+        model = User
+        fields = ["email", "username", "gender", "date_of_birth"]
+
+    def clean_password2(self):
+        
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = User
+        fields = ["email", "password", "username", "gender", "date_of_birth", "is_active", "is_admin"]
+
+
+class UserAdmin(BaseUserAdmin):
+    
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ["email", "username", "is_admin"]
+    list_filter = ["is_admin"]
+    fieldsets = [
+        (None, {"fields": ["email", "password"]}),
+        ("Personal info", {"fields": ["username", "gender", "date_of_birth", "preference", "introduction", "image"]}),
+        ("Permissions", {"fields": ["is_admin"]}),
+    ]
+    
+    add_fieldsets = [
+        (
+            None,
+            {
+                "classes": ["wide"],
+                "fields": ["email", "password1", "password2", "username", "gender", "date_of_birth"],
+            },
+        ),
+    ]
+    search_fields = ["email", "username"]
+    ordering = ["email"]
+    filter_horizontal = []
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ('email',)
+        else:
+            return ()
+
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
