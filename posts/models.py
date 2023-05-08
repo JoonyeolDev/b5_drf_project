d70d619c284fc95ecac49b0b25a88fa742a4909d
Kim Gwang woon <misili304@naver.com>
2023-05-08 14:56:01 +0900

    Feat/ create Posting model, register to admin

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Posting(models.Model):
+    title = models.CharField("제목", max_length=50)
+    posting_content = models.TextField("내용")
+    created_at =  models.DateTimeField("생성일", auto_now_add=True)
+    updated_at =  models.DateTimeField("수정일", auto_now=True)
+    posting_image = models.ImageField("이미지", null=True, upload_to="", blank=True)
+    
