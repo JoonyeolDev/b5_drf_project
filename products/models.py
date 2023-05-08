008dae38e1f0022311482ec04a15e7a185ab39d9
Yoon joonyeol <raoneli@naver.com>
2023-05-08 19:33:33 +0900

    add Product model likes, also serializer

--- products/models.py
+++ products/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from users.models import User
 # Create your models here.
 
 class Product(models.Model):
@@ -9,6 +9,7 @@ class Product(models.Model):
     brand = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    likes = models.ManyToManyField(User, related_name="like_products")
 
     def __str__(self):
         return str(self.name)
--- products/serializers.py
+++ products/serializers.py
@@ -2,10 +2,14 @@ from rest_framework import serializers
 from products.models import Product
 
 class ProductListSerializer(serializers.ModelSerializer):
-    
+    likes_count = serializers.SerializerMethodField()
+
+    def get_likes_count(self,obj):
+        return obj.likes.count()
+
     class Meta:
         model = Product
-        fields = ('name','image','brand')
+        fields = ('name','image','brand','likes_count')
 
 
 class ProductSerializer(serializers.ModelSerializer):
