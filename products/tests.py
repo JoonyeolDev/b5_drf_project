de2cd5413e73e6db799d1b2b42d5e0b55b510631
Yoon joonyeol <raoneli@naver.com>
2023-05-10 01:00:37 +0900

    add product update and delete test

--- products/tests.py
+++ products/tests.py
@@ -19,6 +19,91 @@ def get_temporary_image(temp_file):
     image.save(temp_file,'png')
     return temp_file
 
+
+class ProductDeleteTest(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.admin = User.objects.create_superuser('admin@naver.com','admin_joonyeol','password')
+        cls.admin_data = {'email':'admin@naver.com','password':'password'}
+        cls.user = User.objects.create_user('test@naver.com','test_joonyeol','password')
+        cls.user_data = {'email':'test@naver.com','password':'password'}
+        cls.product_data = {'name':'product test name','introdution':'product test introduction','brand':'product test brand'}
+        # Faker를 사용해서 랜덤 product를 만듦
+        cls.faker = Faker()
+        cls.product = Product.objects.create(name=cls.faker.sentence(), introdution=cls.faker.text())
+
+    # admin, user의 access token을 받아옴
+    def setUp(self):
+        self.admin_access_token = self.client.post(reverse('token_obtain_pair'), self.admin_data).data['access']
+        self.user_access_token = self.client.post(reverse('token_obtain_pair'), self.user_data).data['access']
+
+    # 로그인 없이 put요청 보내면 401 확인
+    def test_fail_if_not_logged_in(self):
+        url = reverse("product_detail", kwargs={"product_id":self.product.id})
+        response = self.client.delete(url, self.product_data)
+        self.assertEqual(response.status_code, 401)
+
+    # 로그인해도 admin이 아닌 user가 put요청 보내면 403 확인
+    def test_fail_if_not_admin(self):
+        response = self.client.delete(
+            path=reverse("product_detail", kwargs={"product_id":self.product.id}),
+            data=self.product_data,
+            HTTP_AUTHORIZATION=f"Bearer {self.user_access_token}",
+            )
+        self.assertEqual(response.status_code, 403)
+
+    # admin이 put요청 보내면 204 확인
+    def test_update_product(self):
+        response = self.client.delete(
+            path=reverse("product_detail", kwargs={"product_id":self.product.id}),
+            data=self.product_data,
+            HTTP_AUTHORIZATION=f"Bearer {self.admin_access_token}",
+            )
+        self.assertEqual(response.status_code, 204)
+
+
+class ProductUpdateTest(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.admin = User.objects.create_superuser('admin@naver.com','admin_joonyeol','password')
+        cls.admin_data = {'email':'admin@naver.com','password':'password'}
+        cls.user = User.objects.create_user('test@naver.com','test_joonyeol','password')
+        cls.user_data = {'email':'test@naver.com','password':'password'}
+        cls.product_data = {'name':'product test name','introdution':'product test introduction','brand':'product test brand'}
+        # Faker를 사용해서 랜덤 product를 만듦
+        cls.faker = Faker()
+        cls.product = Product.objects.create(name=cls.faker.sentence(), introdution=cls.faker.text())
+
+    # admin, user의 access token을 받아옴
+    def setUp(self):
+        self.admin_access_token = self.client.post(reverse('token_obtain_pair'), self.admin_data).data['access']
+        self.user_access_token = self.client.post(reverse('token_obtain_pair'), self.user_data).data['access']
+
+    # 로그인 없이 put요청 보내면 401 확인
+    def test_fail_if_not_logged_in(self):
+        url = reverse("product_detail", kwargs={"product_id":self.product.id})
+        response = self.client.put(url, self.product_data)
+        self.assertEqual(response.status_code, 401)
+
+    # 로그인해도 admin이 아닌 user가 put요청 보내면 403 확인
+    def test_fail_if_not_admin(self):
+        response = self.client.put(
+            path=reverse("product_detail", kwargs={"product_id":self.product.id}),
+            data=self.product_data,
+            HTTP_AUTHORIZATION=f"Bearer {self.user_access_token}",
+            )
+        self.assertEqual(response.status_code, 403)
+
+    # admin이 put요청 보내면 200 확인
+    def test_update_product(self):
+        response = self.client.put(
+            path=reverse("product_detail", kwargs={"product_id":self.product.id}),
+            data=self.product_data,
+            HTTP_AUTHORIZATION=f"Bearer {self.admin_access_token}",
+            )
+        self.assertEqual(response.status_code, 200)
+
+
 class ProductCreateTest(APITestCase):
     @classmethod
     def setUpTestData(cls):
@@ -32,14 +117,14 @@ class ProductCreateTest(APITestCase):
     def setUp(self):
         self.admin_access_token = self.client.post(reverse('token_obtain_pair'), self.admin_data).data['access']
         self.user_access_token = self.client.post(reverse('token_obtain_pair'), self.user_data).data['access']
-
+        
     # 로그인 없이 post요청 보내면 401 확인
     def test_fail_if_not_logged_in(self):
         url = reverse("product_list")
         response = self.client.post(url, self.product_data)
         self.assertEqual(response.status_code, 401)
 
-    # 로그인해도 admin이 아니면 403 확인
+    # 로그인해도 admin이 아닌 user가 post요청 보내면 403 확인
     def test_fail_if_not_admin(self):
         response = self.client.post(
             path=reverse("product_list"),
